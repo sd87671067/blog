4646c3b5db9100d66e9e3060aadb3f289c238a2f
sd87671067 <160470257+sd87671067@users.noreply.github.com>
2025-12-26 20:10:43 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
+
   images: {
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
 };
 
